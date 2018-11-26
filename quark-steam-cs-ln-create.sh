ef60d19589186c64b38104e786c5a18a084ed83e
Antti Partanen <aehparta@iki.fi>
2018-11-26 23:27:43 +0200

    quark stuff

--- quark-steam-cs-ln-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s "$HOME/.steam/steam/steamapps/common/Half-Life"
-
-if [ ! -f "Half-Life/HL.EXE" ]; then
-	touch "Half-Life/HL.EXE"
-fi
---
+++ quark/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+quark_path="/home/aehparta/.wine/drive_c/Program Files (x86)/QuArK 6.6"
+if [ -d "$quark_path" ]; then
+    ln -s "duges.qrk" "$quark_path/addons/duges.qrk"
+else
+    echo "ERROR: QuArK path not found: $quark_path"
+fi
+
+
+steam_hl_path="$HOME/.steam/steam/steamapps/common/Half-Life"
+if [ -d "$steam_hl_path" ]; then
+    if [ ! -e "Half-Life" ]; then
+        ln -s "$steam_hl_path" "Half-Life"
+    fi
+    if [ ! -f "$steam_hl_path/HL.EXE" ]; then
+	    touch "$steam_hl_path/HL.EXE"
+    fi
+else
+    echo "ERROR: Half-Life installation not found from path: $steam_hl_path"
+fi
+
+echo "If no errors, now set QuArK Half-Life path to Half-Life under this directory"
